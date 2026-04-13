0294d546f39c117be40ad78ab652b212f98b18a7
Gaurav Kumar <gauravkumarssc789@gmail.com>
2026-04-13 20:24:43 +0000

    first commit

---
+++ data_type_conversion.js
@@ -0,0 +1,29 @@
+let Value = "abc"
+console.log(typeof Value);
+
+
+let valuee = Number(Value)
+console.log(typeof valuee);
+
+let getValue = false
+console.log(typeof getValue);
+
+let getvalue = Number(getValue)
+console.log(typeof getvalue)
+console.log(getvalue)
+
+"33" == 33
+"33abc" => NaN
+true ==1 false==0
+
+let value = 45678
+let getvalue = Boolean(value)
+console.log(getvalue)
+
+
+true == 1 false==0
+"gaurav"==true
+"" == false
+
+
+
---
+++ variable.js
@@ -0,0 +1,58 @@
+console.log("gaurav")
+
+let a = 10;
+let b = 5;
+
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b=",a*b);
+console.log("a / b =",a/b);
+console.log("a%b=",a%b);
+
+
+var name = "Gaurav";
+let age = 22;
+console.log(name);
+console.log(age);
+
+
+const city ={
+    name : "rahul"
+}
+city.name = "patna";
+console.log("aara");
+console.log(city)
+
+
+
+
+var name= "raj";
+var name="raju";
+name = "ranaj";
+name="gaurav";
+console.log(name);
+
+
+let name= "lalu";
+name="tejaswi";
+name = "tej";
+console.log(name);
+
+
+const name= "gaurav";
+// name= "ranjan";
+console.log(name);
+
+
+
+{
+    let a = 10;
+    
+    console.log(a);
+
+}
+
+{
+    let a = 20;
+    console.log(a);
+}
