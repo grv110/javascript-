5446c3ff45861436895c4e2306d8fd036a3e2797
Gaurav Kumar <gauravkumarssc789@gmail.com>
2026-04-14 17:58:24 +0000

    container config done

---
+++ loop and string.js
@@ -0,0 +1,83 @@
+// let a = 10;
+// let b = 20;
+// console.log(a);
+// console.log(b);
+// setTimeout(()=>{ console.log("Hello"),2000});
+// for(let i = 0; i<= 10000000; i++)
+// {
+
+// }
+// for(let i = 0; i<= 1000000; i++)
+// {
+
+// }
+
+// console.log("end");
+
+
+// for (let i = 1;i<=4;i++){
+//     console.log("javascript");
+
+
+// let sum =0;
+// for (let i = 1;i<=10;i++){
+//     sum = sum +i;
+// }
+// console.log(sum)
+// console.log("loop end");
+
+
+// const num = prompt("enter the number");
+// let i = 100;
+// for (let i=0;i<=100;i++){
+//     if(i%2==0){
+//         console.log("even number",i);
+//     }
+
+// let gameNum = 10;
+
+// let userNum = prompt("enter the number");
+
+// while (gameNum!=userNum){
+
+//     userNum = prompt("enter the number again ");
+
+    
+// } 
+//  console.log("you won!");
+
+
+
+// string in javaScript//
+
+// let str = "javaScript";
+// console.log(str[4]);
+// console.log(str.length);
+
+
+// let str = "javascript"
+// console.log(str.trim());
+// let name = "Gaurav";
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+// greet();
+// console.log(name);
+
+// var a = 10;
+// function test(){ }
+
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+// greet();
+
+// function greet(){
+//     console.log("Hello");
+// }
+// var a;
+// console.log(a);
+// a = 10;
